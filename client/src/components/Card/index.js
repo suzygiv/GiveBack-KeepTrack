bcd5126c46321ecacbc738e47c46f4a42bcf5140
Alison Lubar <aslubar@gmail.com>
2021-01-30 11:25:50 -0500

    created pages and structured app js file

---
+++ client/src/components/Card/index.js
@@ -0,0 +1,22 @@
+//this will change, and is just a placeholder to make sure the pages work
+
+import React from "react";
+
+function Card(props) {
+    return (
+
+        <div className="card mb-4 text-center">
+            <div className="card-header portfolio-card">
+                Card Header
+            </div>
+            <div className="card-body">
+                <h5 className="card-title">Card Title</h5>
+                <p className="card-text">Card Text
+                </p>
+            </div>
+        </div>
+
+    )
+}
+
+export default Card;
---
+++ client/src/pages/givebackentry.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "../components/Card";
+
+const GiveBack = () => {
+    return (
+        <div>
+            <Card />
+        </div>
+    );
+}
+export default GiveBack;

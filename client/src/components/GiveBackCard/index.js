2edce1b0b8f2c993a19bc6c51309f71a06c71ff9
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-01-30 11:31:17 -0500

    added card and form for giveback page

---
+++ client/src/components/GiveBackCard/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+function GiveBackCard() {
+    return (
+    <div className="card">
+    <div className="card-header">
+          Give Back
+    </div>
+    <div className="content">
+
+
+
+    </div>
+    
+    
+    
+    </div>
+    );
+}
+
+export default GiveBackCard

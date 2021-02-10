d58400845393ee6e5cd6f19c71ef5d3b849cc2fd
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-02-09 20:58:15 -0500

    added to giveback.js model to create columns, added schema.sql and seeds.sql

--- models/giveback.js
+++ models/giveback.js
@@ -2,9 +2,36 @@
 module.exports = function(sequelize, DataTypes) {
   var Giveback = sequelize.define("Giveback", {
 
-    title: {
+    organization: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    
+    category: {
+      // or a num 
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    employeeMatch: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
+    },
+
+    taxDeductible: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
+    
+    },
+    receiptDocumentation: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
+    
+    },
+    
+    amount: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
     }
   });
  
---
+++ scripts/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS gbkt;
+
+CREATE DATABASE gbkt;
+
+

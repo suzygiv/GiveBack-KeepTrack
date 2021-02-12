833cb41fd2d9697650d010a994ddeac8ec7a509a
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-02-11 19:50:55 -0500

    added front end design to log in and sign up forms.

---
+++ client/src/components/LoginSignupNav/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+
+function LoginSignupNav() {
+
+    return (
+<nav className="navbar bg-white">
+  <a className="navbar-brand" href="#">
+  <img
+        alt=""
+        src={process.env.PUBLIC_URL+"/images/logo.png"}
+        to="/welcome"
+        width="60"
+        height="60"
+        className="d-inline-block align-top"
+      />{' '}
+  </a>
+</nav>
+    );
+}
+
+export default LoginSignupNav;

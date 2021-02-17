fca1301814ea8444874454630434b8835b9b2609
suzygiv <suzannegivnish@gmail.com>
2021-02-16 21:14:20 -0500

    changes

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,34 +6,34 @@ import "./style.css";
 
 function Nav() {
 
-  return (<>
-
+    return (<>
+  
     <Navbar bg="white" variant="light">
-      <Navbar.Brand href="/welcome">
-        <img
-          alt=""
-          src={process.env.PUBLIC_URL + "/images/logo.png"}
-          to="/welcome"
-          width="40"
-          height="40"
-          className="d-inline-block align-top"
-        />{' '}
-      </Navbar.Brand>
-
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Link className="nav-link navRight" to="/">Login <span className="sr-only">(current)</span></Link>
-          <Link className="nav-link navRight" to="/givebackentry">Giveback</Link>
-          <Link className="nav-link navRight" to="/keeptrack">Keeptrack</Link>
-          <Link className="nav-link navRight" to="/">Logout</Link>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar.Brand href="/welcome">
+      <img
+        alt=""
+        src={process.env.PUBLIC_URL+"/images/logo.png"}
+        to="/welcome"
+        width="40"
+        height="40"
+        className="d-inline-block align-top"
+      />{' '}
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                <Link className="nav-link navRight"to="/">Login <span className="sr-only">(current)</span></Link>
+                <Link className="nav-link navRight" to="/givebackentry">Giveback</Link>
+                <Link className="nav-link navRight" to="/keeptrack">Keeptrack</Link>
+                <Link className="nav-link navRight" to="/">Logout</Link>
+                
+            </Navbar.Collapse>
+        
     </Navbar>
+   
 
-  </>
-  )
+   
+    </>
+    )
 }
-
 
 export default Nav;

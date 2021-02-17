7f4d103f1e6a30f875d306cedc6b546267027850
suzygiv <suzannegivnish@gmail.com>
2021-02-16 20:01:36 -0500

    cleaned up code

--- client/src/components/Col/index.js
+++ client/src/components/Col/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 function Col(props) {
     const size = props.size.split(" ").map(size => "col-" + size).join(" ");
     return <div className={size}>{props.children}</div>;
--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 function Container(props) {
     return <div className="container" style={props.style}>{props.children}</div>;
 }
